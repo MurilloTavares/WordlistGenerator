5ba742a42191e50c555ab54ac757c63d57ac8939
MurilloTavares <murillo_eum@hotmail.com>
2018-11-10 11:37:37 -0300

    Refatorando WordlistGeneratorAB

--- src/util/WordlistGeneratorAB.java
+++ src/util/WordlistGeneratorAB.java
@@ -5,9 +5,11 @@ import java.util.List;
 
 public class WordlistGeneratorAB {
 	
-	public static List<String> generate(List<Character> conjuntoA, int sizeA, List<Character> conjuntoB, int sizeB) {		
+	private List<String> wordlist;
+	
+	public List<String> generate(List<Character> conjuntoA, int sizeA, List<Character> conjuntoB, int sizeB) {		
 		
-		List<String> wordlist = new ArrayList<>();
+		this.wordlist = new ArrayList<>();
 		
 		// Buscando combinacoes para A e para B separados
 		WordlistGenerator generator = new WordlistGenerator();
@@ -16,28 +18,61 @@ public class WordlistGeneratorAB {
 		
 		// Inserindo valores de B dentro de A para todas as posicoes possiveis
 		for(String wordA : wordlistA) {
-			int possiblesPos = wordA.length() + 1;
+			
+			//int possiblesPos = wordA.length() + 1;
 			
 			for(String wordB: wordlistB) {
 				
-				char b1 = wordB.charAt(0);
-				char b2 = wordB.charAt(1);
+				startInsertChain(wordA, wordB, sizeB);
 				
-				for(int pos1 = 0; pos1 < possiblesPos; pos1++) {
+				/*for(int pos1 = 0; pos1 < possiblesPos; pos1++) {	
+					
+					char b1 = wordB.charAt(0);
+					String newStringA = insertIntoPosition(wordA, b1, pos1);
+					
 					for(int pos2 = pos1 + 1; pos2 < possiblesPos+1; pos2++) {
+
+						char b2 = wordB.charAt(1);
+						String newStringB = insertIntoPosition(newStringA, b2, pos2);
 						
-						String newString = insertIntoPosition(wordA, b1, pos1);
-						newString = insertIntoPosition(newString, b2, pos2);
-						
-						wordlist.add(newString);						
+						wordlist.add(newStringB);		
 					}
-				}
+				}*/
 			}
 		}		
 		return wordlist;
 	}
 	
-	public static String insertIntoPosition(String string, char letra, int position) {		
+	private void startInsertChain(String wordA, String wordB, int numChain){		
+		
+		insertChain(0, wordA.length()+1, wordA, wordB, 0, numChain);
+		
+	}
+	
+	private void insertChain(int startPos, int finalPos, String wordToInsert, String wordToLetra, int letraPos, int remaingChain) {
+		
+		remaingChain--;
+		
+		for(int pos = startPos; pos < finalPos; pos++) {			
+			
+			char letra = wordToLetra.charAt(letraPos);
+			String newWord = insertIntoPosition(wordToInsert, letra, pos);
+			
+			// Caso precise continuar interacao
+			if(remaingChain > 0) {
+					
+				insertChain(pos+1, finalPos+1, newWord, wordToLetra, letraPos+1, remaingChain);
+				
+			} else {
+				// Caso ultima interacao
+				this.wordlist.add(newWord);
+			}
+								
+		}
+		
+	}
+	
+	private String insertIntoPosition(String string, char letra, int position) {		
 		if(position<=0) {
 			return letra+string;
 		}
